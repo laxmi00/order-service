7500d511f13fad9eaf3bbb5519d5e4fd9480fd6e
laxmi00 <lkp.laxmi277@gmail.com>
2022-09-03 02:47:28 +0530

    EUREKA SERVER

---
+++ Registry-server/src/test/java/com/javaMS/SR/api/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaMS.SR.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
